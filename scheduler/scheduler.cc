341a337212a74121dcc1fd6411b3038d46a1a24a
juseongjin <cyse3062@naver.com>
2023-08-31 01:52:56 +0900

    mkdir yolo, efficient, mobilenet subgraph directory
     Changes to be committed:
    	modified:   scheduler/scheduler.cc

--- scheduler/scheduler.cc
+++ scheduler/scheduler.cc
@@ -1,7 +1,7 @@
 #include "tensorflow/lite/tf_scheduler.h"
 
 #define SCHEDULER_SOCK "/home/nvidia/TfLite_apps/sock/scheduler"
-#define PARTITIONING_PARAMS "/home/nvidia/TfLite_apps/params/partitioning_params"
+#define PARTITIONING_PARAMS "/home/nvidia/TfLite_apps/params/subgraph/[model_type]/[partitioning_plan]"
 
 int main(){
   tflite::TfScheduler scheduler(SCHEDULER_SOCK, PARTITIONING_PARAMS);
