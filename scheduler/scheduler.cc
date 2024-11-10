e63708e1ae3850b5dbb64fd02e4e974aabc1180d
Minsung Kim <alstjd025@naver.com>
2024-11-10 13:17:36 +0900

    hanges to be committed: 	modified:   TfLite_app 	new file:   scheduler/final_multi_level/nano/efficientnet/cpu.yaml 	new file:   scheduler/final_multi_level/nano/efficientnet/gpu.yaml 	modified:   scheduler/final_multi_level/nano/efficientnet/level_0.yaml 	modified:   scheduler/final_multi_level/nano/efficientnet/level_1.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_1_cpu.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_1_gpu.yaml 	modified:   scheduler/final_multi_level/nano/efficientnet/level_2.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_2_cpu.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_2_gpu.yaml 	modified:   scheduler/final_multi_level/nano/efficientnet/level_3.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_3_cpu.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_3_gpu.yaml 	modified:   scheduler/final_multi_level/nano/efficientnet/level_4.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_4_cpu.yaml 	deleted:    scheduler/final_multi_level/nano/efficientnet/level_4_gpu.yaml 	modified:   scheduler/scheduler 	modified:   scheduler/scheduler.cc

--- scheduler/scheduler.cc
+++ scheduler/scheduler.cc
@@ -1,4 +1,6 @@
 #include "tensorflow/lite/tf_scheduler.h"
+#include <stdlib.h> 
+#include <cstdlib>
 
 // Note: target board config
 #define nx
@@ -32,19 +34,24 @@
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     std::cout
-        << "ERROR on argument. Usage : ./scheduler partitioning_param_1 partitioning_param_2 ..."
+        << "ERROR on argument. Usage : ./scheduler [recovery_on/off(1, 0)] partitioning_param_1 partitioning_param_2 ..."
         << "\n";
     exit(-1);
   }
   // Parse parameters
   int params = argc;
   std::vector<std::string> param_file_names;
-  for(int i=1; i<params; ++i){
+  for(int i=2; i<params; ++i){
     param_file_names.push_back(argv[i]);
   }
-
+  bool recovery = false;
+  if(atoi(argv[1]) == 0){
+    recovery = false;
+  }else{
+    recovery = true;
+  }
   tflite::TfScheduler scheduler(SCHEDULER_SOCK, SCHEDULER_SOCK_2, SCHEDULER_ENGINE, param_file_names);
-  scheduler.Work();
+  scheduler.Work(recovery);
 
   return 0;
 }
